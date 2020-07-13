f2aff81c69d8dd6caa46665d83940689b4bf6298
Jarrett Yeap <hooitongx@gmail.com>
2020-07-14 01:29:50 +0800

    Scaffolded plugin for adding video from server

---
+++ packages/ckeditor5-video/src/video.js
@@ -0,0 +1,26 @@
+import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
+
+import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
+
+export default class Video extends Plugin {
+    init() {
+        const editor = this.editor;
+
+        editor.ui.componentFactory.add( 'insertImage', locale => {
+            const view = new ButtonView( locale );
+
+            view.set( {
+                label: 'Insert image',
+                icon: imageIcon,
+                tooltip: true
+            } );
+
+            // Callback executed once the image is clicked.
+            view.on( 'execute', () => {
+                const imageURL = prompt( 'Image URL' );
+            } );
+
+            return view;
+        } );
+    }
+}
